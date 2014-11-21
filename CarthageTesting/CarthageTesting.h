54a313c92e1474bc0e9b340bc2cb1924908c5cf9
RPeres <rui.peres@williamhill.com>
2014-11-21 14:15:13 +0000

    Added a CarthageTesting scheme

---
+++ CarthageTesting/CarthageTesting.h
@@ -0,0 +1,19 @@
+//
+//  CarthageTesting.h
+//  CarthageTesting
+//
+//  Created by Rui Peres on 21/11/2014.
+//  Copyright (c) 2014 STHTTPRequest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CarthageTesting.
+FOUNDATION_EXPORT double CarthageTestingVersionNumber;
+
+//! Project version string for CarthageTesting.
+FOUNDATION_EXPORT const unsigned char CarthageTestingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CarthageTesting/PublicHeader.h>
+
+
